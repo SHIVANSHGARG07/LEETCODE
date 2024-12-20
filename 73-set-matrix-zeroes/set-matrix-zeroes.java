2ed18dcc2144e25c370cd480477a634487af81b6
SHIVANSHGARG07 <122355583+SHIVANSHGARG07@users.noreply.github.com>
2024-12-20 10:57:17 +0530

    Time: 2 ms (19.13%) | Memory: 45.6 MB (63.37%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,22 +1,20 @@
+import java.util.HashSet;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        // Lists to store rows and columns to be zeroed
-        List<Integer> zeroRows = new ArrayList<>();
-        List<Integer> zeroCols = new ArrayList<>();
+        // HashSets to store rows and columns to be zeroed
+        HashSet<Integer> zeroRows = new HashSet<>();
+        HashSet<Integer> zeroCols = new HashSet<>();
         
         // Step 1: Identify rows and columns that need to be zeroed
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    if (!zeroRows.contains(i)) {
-                        zeroRows.add(i);  // Add the row index to zeroRows list
-                    }
-                    if (!zeroCols.contains(j)) {
-                        zeroCols.add(j);  // Add the column index to zeroCols list
-                    }
+                    zeroRows.add(i);  // Add the row index to zeroRows set
+                    zeroCols.add(j);  // Add the column index to zeroCols set
                 }
             }
         }
