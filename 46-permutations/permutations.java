0b46aa125722e40bf5a314bc3ce850c039218ef8
SHIVANSHGARG07 <122355583+SHIVANSHGARG07@users.noreply.github.com>
2024-12-22 01:03:08 +0530

    Time: 2 ms (43.50%) | Memory: 44.8 MB (11.62%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,26 @@
 class Solution {
-    List<List<Integer>>two = new ArrayList<>();
-    List<Integer>one = new ArrayList<>();
-
-    public void backtrack(int[] nums,HashSet<Integer>used){
+    public void backtrack(int[] nums, HashSet<Integer> used, List<Integer> one, List<List<Integer>> two) {
         int n = nums.length;
-     if(one.size()==n){
-        two.add(new ArrayList<>(one));
-     }
-     for(int i=0;i<n;i++){
-        if(used.contains(i)) continue;
-        used.add(i);
-        one.add(nums[i]);
+        if (one.size() == n) {
+            two.add(new ArrayList<>(one));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (used.contains(i)) continue;
+            used.add(i);
+            one.add(nums[i]);
 
-        backtrack(nums,used);
-        used.remove(i);
-        one.remove(one.size()-1);
-     }
+            backtrack(nums, used, one, two);
+            used.remove(i);
+            one.remove(one.size() - 1);
+        }
     }
+
     public List<List<Integer>> permute(int[] nums) {
-        HashSet<Integer>used = new HashSet<>();
-        backtrack(nums,used);
+        List<List<Integer>> two = new ArrayList<>();
+        List<Integer> one = new ArrayList<>();
+        HashSet<Integer> used = new HashSet<>();
+        backtrack(nums, used, one, two);
         return two;
     }
 }
