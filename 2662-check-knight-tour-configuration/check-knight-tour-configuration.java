f0c5cb241f6b9cd4d9e66e809dec5676538ae31c
SHIVANSHGARG07 <122355583+SHIVANSHGARG07@users.noreply.github.com>
2024-12-22 18:26:49 +0530

    Time: 1 ms (92.35%) | Memory: 44.4 MB (51.28%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.java
@@ -2,27 +2,24 @@ class Solution {
    int[] dirx = {1,2,-1,-2,1,2,-1,-2};
     int[] diry = {2,1,2,1,-2,-1,-2,-1};
 
-    private boolean check(int i,int j,int n,int move,int[][] grid,boolean[][] vis){
-        if(i<0 || j<0 || i>=n || j>=n || grid[i][j]!=move || vis[i][j]) return false;
+    private boolean check(int i,int j,int n,int move,int[][] grid){
+        if(i<0 || j<0 || i>=n || j>=n || grid[i][j]!=move ) return false;
 
         if(move==n*n-1) return true;
-        vis[i][j]=true;
 
         for(int k=0;k<8;k++){
             int nr = i+dirx[k];
             int nc = j+diry[k];
 
-            if(check(nr,nc,n,move+1,grid,vis)){
+            if(check(nr,nc,n,move+1,grid)){
                 return true;
             }
         }
-                vis[i][j]=false;
 
         return false;
     }
     public boolean checkValidGrid(int[][] grid) {
         int n = grid.length;
-        boolean[][] vis = new boolean[n][n];
-        return check(0,0,n,0,grid,vis);
+        return check(0,0,n,0,grid);
     }
 }
