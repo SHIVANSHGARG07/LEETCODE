2a2df4a636f26290b3af0de1aec6579a3a48c894
Shivansh Garg <shivanshgarg587@gmail.com>
2024-12-31 14:37:48 +0530

    Time: 2 ms (58.55%) | Memory: 42.3 MB (29.44%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -4,7 +4,7 @@ class Solution {
         int n = s.length();
         for(int i=0;i<n;i++){
           if(s.charAt(i)==digit){
-            String newString = s.substring(0,i)+s.substring(i+1,n);
+            String newString = s.substring(0,i)+s.substring(i+1);
             ans = ans.compareTo(newString)<0?newString:ans;
           }
 
