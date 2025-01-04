d50214d54d226c8bc2bed24634b07cad656e4bd7
Shivansh Garg <shivanshgarg587@gmail.com>
2025-01-04 10:56:10 +0530

    Time: 159 ms (77.22%) | Memory: 45.4 MB (60.21%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,37 @@
+class Pair{
+   private int first;
+   private int second;
+   public Pair(int first,int second){
+    this.first=first;
+    this.second = second;
+   }
+
+}
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int[] first = new int[26];
+        int [] second = new int[26];
+        Arrays.fill(first,-1);
+        Arrays.fill(second,-1);
+
+        int n = s.length();
+
+        for(int i=0;i<n;i++){
+            int ch=s.charAt(i)-'a';
+            if(first[ch]==-1){
+                first[ch]=i;
+            }
+            second[ch]=i;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(first[i]==-1) continue;
+            HashSet<Character>st = new HashSet<>();
+            for(int idx = first[i]+1;idx<second[i];idx++){
+                st.add(s.charAt(idx));
+            }
+            count+=st.size();
+        }
+        return count;
+    }
+}
