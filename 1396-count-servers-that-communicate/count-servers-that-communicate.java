ec5c11d533a9efc4a70716b8ed2e767c54ff9780
Shivansh Garg <shivanshgarg587@gmail.com>
2025-01-23 17:31:06 +0530

    Time: 2 ms (100.00%) | Memory: 52.8 MB (20.85%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countServers(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] r = new int[m];
+        int[] c= new int[n];
+
+        for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1){
+                r[i]++;
+                c[j]++;
+            }
+        }
+        }
+    int ans =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    if(r[i]>1 || c[j]>1)
+                    ans++;
+                }
+            }
+        }
+        return ans;
+
+    }
+}
