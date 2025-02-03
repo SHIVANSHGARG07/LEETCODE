84a82bba8b8fddd8f20499adfa23ca7a62d3dcc1
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-03 18:33:49 +0530

    Time: 14 ms (14.23%) | Memory: 47.5 MB (28.22%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,33 @@
+class Solution {
+    private int binary(int cap, int[] weights) {
+        int days = 1, load = 0;
+        int n = weights.length;
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days++;
+                load = weights[i];  
+            } else {
+                load += weights[i];  // add to the current shipment
+            }
+        }
+        return days;
+    }
+
+    public int shipWithinDays(int[] weights, int k) {
+        int maxy = Arrays.stream(weights).max().getAsInt();
+        int totalSum = Arrays.stream(weights).sum();
+
+        int left = maxy, right = totalSum;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int days = binary(mid, weights);
+            
+            if (days <= k) {
+                right = mid;  // Try a smaller capacity to minimize the largest load
+            } else {
+                left = mid + 1;  // Increase the capacity
+            }
+        }
+        return left;  // Return the minimum capacity found
+    }
+}
