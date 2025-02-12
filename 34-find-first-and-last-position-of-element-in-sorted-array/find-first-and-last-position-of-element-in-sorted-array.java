e1bef41bd577ecc86e9d0ad0fcc2f746878a2148
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-12 16:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (16.80%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int s=0,e=nums.length-1;
-        int first =-1,last=-1;
+        int first = func(nums,target,true);
+        int last = func(nums,target,false);
 
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(nums[mid]==target){
-                first = mid;
-                e=mid-1;
-            }
-            else if(nums[mid]<target){
-                s=mid+1;
-            }
-            else{
-               e=mid-1; 
-            }
+        int[] arr = new int[2];
+        arr[0]=first;
+        arr[1]=last;
+        return arr;
         }
-        
-        s=0;
-        e=nums.length-1;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(nums[mid]==target){
-                last = mid;
-                s=mid+1;
+        private int func(int[] nums,int target,boolean flag){
+
+            int l = 0,r=nums.length-1;
+            int ans=-1;
+            if(flag){
+              while(l<=r){
+                int mid = l+(r-l)/2;
+                if(nums[mid]==target) {ans = mid ; r=mid-1;}
+                else if(nums[mid]<target) l=mid+1;
+                else r=mid-1;
+              }
             }
-            else if(nums[mid]<target){
-                s=mid+1;
+              else{
+                while(l<=r){
+                int mid = l+(r-l)/2;
+                if(nums[mid]==target) {ans = mid ; l=mid+1;}
+                else if(nums[mid]<target) l=mid+1;
+                else r=mid-1;
+              }
+              
             }
-            else{
-               e=mid-1; 
-            }
+            return ans;
         }
-        int[]res = new int[2];
-        res[0]=first;
-        res[1]=last;
-        return res;
-    }
 }
