1cd2dd372be0ee67efa30604bd801c7cac3f75c5
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-17 11:22:26 +0530

    Time: 3 ms (69.13%) | Memory: 41.3 MB (95.47%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,29 +1,31 @@
+import java.util.*;
+
 class Solution {
-    private void backtrack(String tiles,HashSet<String>st,StringBuilder sb,boolean[] vis){
-        if(sb.length()>0){
-            st.add(sb.toString());
-            
+    private void backtrack(char[] tiles, int[] count, int length, int[] res) {
+        if (length > 0) {
+            res[0]++; // Count the valid sequence
         }
 
-        for(int i=0;i<tiles.length();i++){
-            if(vis[i]){
-                continue;
-            }
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0) continue; // Skip exhausted characters
 
-            sb.append(tiles.charAt(i));
-            vis[i]=true;
-
-            backtrack(tiles,st,sb,vis);
-
-            sb.deleteCharAt(sb.length()-1);
-            vis[i]=false;
+            count[i]--; // Use this character
+            backtrack(tiles, count, length + 1, res);
+            count[i]++; // Backtrack to restore count
         }
     }
+
     public int numTilePossibilities(String tiles) {
-        HashSet<String>st = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
-        boolean[] vis = new boolean[tiles.length()];
-        backtrack(tiles,st,sb,vis);
-        return st.size();
+        char[] tileArray = tiles.toCharArray();
+        Arrays.sort(tileArray); // Sort to group duplicates together
+
+        int[] count = new int[26]; // Since 'A'-'Z' are max 26 characters
+        for (char c : tileArray) {
+            count[c - 'A']++; // Store frequency
+        }
+
+        int[] res = new int[1]; // Using array to store result (pass-by-reference)
+        backtrack(tileArray, count, 0, res);
+        return res[0]; // Return the total count
     }
 }
