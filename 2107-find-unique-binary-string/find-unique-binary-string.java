0f8cd760a74a8a22ae7883aef37a05d0ad7ecb2e
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-20 09:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (77.55%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,41 +1,11 @@
 class Solution {
-    private static void backtrack(int idx,StringBuilder sb,int n ,List<String>list){
-        if(idx==n){
-            list.add(sb.toString());
-            return;
-        }
-
-        sb.append('0');
-        backtrack(idx+1,sb,n,list);
-        sb.deleteCharAt(sb.length()-1);
-
-        sb.append('1');
-        backtrack(idx+1,sb,n,list);
-        sb.deleteCharAt(sb.length()-1);
-
-    }
-
     public String findDifferentBinaryString(String[] nums) {
-        
-        int n = nums[0].length();
-        List<String>ans = new ArrayList<>();
-        HashSet<String>st = new HashSet<>();
-        
         StringBuilder sb = new StringBuilder();
-
-        backtrack(0,sb,n,ans);
-
-        for(int i=0;i<nums.length;i++){
-            st.add(nums[i]);
+        
+        for (int i = 0; i < nums.length; i++) {
+            sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
-        for(String word:ans){
-            if(!st.contains(word)){
-                return word;
-            }
-        }
-        return "";
-
-
-
+        
+        return sb.toString();
     }
 }
