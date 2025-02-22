5b29cc23897c1f36dfdc0ff9cef7b7a9891755f7
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-22 10:09:49 +0530

    Time: 1 ms (71.21%) | Memory: 42.4 MB (34.17%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -4,14 +4,17 @@ class Solution {
             return "";
         }
         
-        // Find the GCD of the lengths
         int gcdLength = gcd(str1.length(), str2.length());
         
-        // The common divisor string is the prefix of str1 up to gcdLength
         return str1.substring(0, gcdLength);
     }
     private int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        if(b==0){
+            return a;
+        }
+        else{
+            return gcd(b,a%b);
+        }
     }
     
     
