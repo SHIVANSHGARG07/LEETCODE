9f45ced5096fccc163068fb7c0488fcb82989537
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-23 13:12:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (46.46%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,23 +1,32 @@
 class Solution {
-    public int compareVersion(String s1, String s2) {
-        String[] a1 = s1.split("\\.");
-        String[] a2 = s2.split("\\.");
+    public int compareVersion(String version1, String version2) {
+        int i = 0, j = 0;
+        int n1 = version1.length(), n2 = version2.length();
 
-        int maxLen = Math.max(a1.length,a2.length);
+        while (i < n1 || j < n2) {
+            int num1 = 0, num2 = 0;
 
-        for(int i=0;i<maxLen;i++){
-            int num1 = (i<a1.length) ? Integer.parseInt(a1[i]):0;
-            int num2 = (i<a2.length) ? Integer.parseInt(a2[i]):0;
-
-            if(num1>num2){
-                return 1;
-            }
-            if(num2>num1){
-                return -1;
+            // Extract number from version1
+            while (i < n1 && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
             }
 
+            // Extract number from version2
+            while (j < n2 && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+
+            // Compare numbers
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+
+            // Move past the dot (if exists)
+            i++;
+            j++;
         }
-        return 0;
-    }
 
+        return 0; // Versions are equal
+    }
 }
