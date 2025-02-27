ab1976fef5247a31da98eaa7d592d2fbbab579f1
Shivansh Garg <shivanshgarg587@gmail.com>
2025-02-27 11:01:13 +0530

    Time: 18 ms (32.02%) | Memory: 56.9 MB (37.64%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
@@ -1,37 +1,54 @@
 class Solution {
-    public long func(int[] nums,int[] cost,int ele){
-      long res =0;
-      for(int i=0;i<nums.length;i++){
-        res+=(long) Math.abs(nums[i]-ele)*cost[i];
-      }
-      return res;
-
-    }
     public long minCost(int[] nums, int[] cost) {
-        long ans=Long.MAX_VALUE;
+     // cond 1  // if not any ele is diff return 0
+      //  cond 2 // must be less than in O(n2)
 
-        int min = Integer.MAX_VALUE;
-       int max = Integer.MIN_VALUE; 
+      // sort both arrays but in relational basis 
+      // find weighted mean like prefixSum for cost array and where array exceed sum than half at that 
+      // at that index it sholud be the target
 
-       for(int num:nums){
-        min = Math.min(min,num);
-        max = Math.max(max,num);
-       }
+     int n = nums.length;
+      int[][] pairs = new int[n][2];
 
-       while(min<=max){
-        int mid = min+(max-min)/2;
-        long cost1 = func(nums,cost,mid);
-        long cost2 = func(nums,cost,mid+1);
+      for(int i=0;i<n;i++){
+        pairs[i][0]=nums[i];
+        pairs[i][1]=cost[i];
+      }
+      Arrays.sort(pairs,(a,b)->Integer.compare(a[0],b[0]));
 
-        ans = Math.min(cost1,cost2);
+      long sum=0;
 
-        if(cost2>cost1){
-            max = mid-1;
+      for(int num:cost){
+        sum+=num;
+      }
+
+      long curr =0;
+      int idx=-1;
+
+      for(int i=0;i<n;i++){
+        curr+=pairs[i][1];
+
+        if(curr*2>=sum){
+            idx=i;
+            break;
         }
-        else{
-            min = mid+1;
-        }
-       }
-       return ans == Integer.MAX_VALUE?0:ans;
+      }
+
+      // now i have to convert to idxth ele 
+      int target = pairs[idx][0];
+      long ans=0;
+
+      for(int i=0;i<idx;i++){
+        ans += (long) (target-pairs[i][0]) * pairs[i][1];
+      }
+
+      for(int i=idx+1;i<n;i++){
+        ans+=(long) (pairs[i][0]-target)*pairs[i][1];
+      }
+
+
+
+return ans;
+
     }
 }
