7b6003b0a1fa3915e4333e11332cc67eb4e003a2
Shivansh Garg <shivanshgarg587@gmail.com>
2025-03-03 11:32:06 +0530

    Time: 1 ms (82.08%) | Memory: 41.8 MB (63.38%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int memo(String s,int[] dp,int idx){
+        int n = s.length();
+        if(idx==n) return 1;
+        if(s.charAt(idx)=='0') return 0;
+        if(dp[idx]!=-1) return dp[idx];
+
+        int res =memo(s,dp,idx+1);
+        int res2=0;
+
+        if(idx<s.length()-1){
+            int temp = Integer.parseInt(s.substring(idx,idx+2));
+            if(temp<=26){
+                res2+=memo(s,dp,idx+2);
+            }
+        }
+        return dp[idx]=res+res2;
+
+    }
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+
+        Arrays.fill(dp,-1);
+        return memo(s,dp,0);
+
+
+    }
+}
