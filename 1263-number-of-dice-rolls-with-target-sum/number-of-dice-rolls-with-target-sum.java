5131bb8bd92d131e379dd559f3c5f714ab102551
Shivansh Garg <shivanshgarg587@gmail.com>
2025-03-03 15:15:05 +0530

    Time: 26 ms (14.21%) | Memory: 41.7 MB (83.27%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,21 +1,21 @@
 class Solution {
-        int mod = 1000000007;
+    public int numRollsToTarget(int n, int m, int target) {
+                int mod = 1000000007;
 
-    public int numRollsToTarget(int n, int m, int k) {
-        int[][] dp = new int[n+1][k+1];
-
-        dp[0][0]=1;
-
+        int[][] dp = new int[n+1][target+1];
+     dp[0][0]=1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=k;j++){
-                for(int x=1;x<=m;x++){
-                    if(j-x>=0){
-                    dp[i][j]=( dp[i][j]+dp[i-1][j-x] )%mod;
+            for(int j=1;j<=target;j++){
+                for(int k=1;k<=m;k++){
+                    if(j-k>=0){
+                     dp[i][j]  =  (dp[i][j]+dp[i-1][j-k])%mod;
                     }
                 }
             }
         }
-        return dp[n][k];
+        return dp[n][target];
+
+
 
     }
 }
