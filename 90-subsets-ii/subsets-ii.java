86aeed96b126b0bacbf61d30ed6c9ea3bdb7fc7c
Shivansh Garg <shivanshgarg587@gmail.com>
2025-03-04 16:53:15 +0530

    Time: 4 ms (13.32%) | Memory: 43.7 MB (44.87%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,26 @@
 class Solution {
-   private void backtrack(int[] nums, int idx, List<Integer> list, Set<List<Integer>> ans) {
-        ans.add(new ArrayList<>(list)); // Store unique subset
-
-        for (int i = idx; i < nums.length; i++) {
-            list.add(nums[i]);
-            backtrack(nums, i + 1, list, ans);
-            list.remove(list.size() - 1); // Backtrack
+    HashSet<List<Integer>>st = new HashSet<>();
+   private void func(int[] nums,int idx,List<Integer>list){
+    if(idx>=nums.length){
+        if(!st.contains(list)){
+        st.add(new ArrayList<>(list));
         }
+        return;
     }
-
+    list.add(nums[idx]);
+    func(nums,idx+1,list);
+    list.remove(list.size()-1);
+    func(nums,idx+1,list);
+    
+   }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums); // Sort to handle duplicates
-        Set<List<Integer>> ans = new HashSet<>();
-        backtrack(nums, 0, new ArrayList<>(), ans);
-        return new ArrayList<>(ans); // Convert HashSet to List
+        List<List<Integer>>ans = new ArrayList<>();
+        Arrays.sort(nums);
+        func(nums,0,new ArrayList<>());
+
+        for(List<Integer>list:st){
+            ans.add(list);
+        }
+        return ans;
     }
 }
