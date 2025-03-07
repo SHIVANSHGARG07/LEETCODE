9cb929ef622816a071a396be43a2109ef282c9ea
Shivansh Garg <shivanshgarg587@gmail.com>
2025-03-07 11:28:18 +0530

    Time: 325 ms (75.18%) | Memory: 44.9 MB (74.21%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    int count=0;
+    private void func(int[] nums,int idx,int k,HashMap<Integer,Integer>mp){
+        if(idx>=nums.length){
+            count++;
+            return;
+        }
+
+        if(!mp.containsKey(nums[idx]-k) && !mp.containsKey(nums[idx]+k)){
+            mp.put(nums[idx],mp.getOrDefault(nums[idx],0)+1);
+            func(nums,idx+1,k,mp);
+            mp.put(nums[idx],mp.get(nums[idx])-1);
+            if(mp.get(nums[idx])==0){
+                mp.remove(nums[idx]);
+            }
+        }
+
+        func(nums,idx+1,k,mp);
+    }
+    public int beautifulSubsets(int[] nums, int k) {
+        HashMap<Integer,Integer>mp = new HashMap<>();
+        func(nums,0,k,mp);
+        return count-1;
+    }
+}
