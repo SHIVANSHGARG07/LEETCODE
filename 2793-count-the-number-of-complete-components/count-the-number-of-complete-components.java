7e246d47874fc440c5245dfa76fa98024040edba
Shivansh Garg <shivanshgarg587@gmail.com>
2025-03-22 08:52:20 +0530

    Time: 11 ms (57.88%) | Memory: 45.4 MB (34.42%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void dfs(List<List<Integer>>adj,int src,boolean[] vis,List<Integer>component){
+        component.add(src);
+     vis[src]=true;
+
+     for(int ne:adj.get(src)){
+        if(!vis[ne]){
+            dfs(adj,ne,vis,component);
+        }
+     }
+    }
+    public int countCompleteComponents(int n, int[][] edges) {
+        // each component contains n-1 edges
+        // firstly find connected components using dfs
+        List<List<Integer>>adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge:edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+ int ans=0;
+        boolean[] vis = new boolean[n];
+        for(int i=0;i<n;i++){
+
+            if(!vis[i]){
+                List<Integer>component = new ArrayList<>();
+
+                dfs(adj,i,vis,component);
+                int edgeCount=0;
+                for(int node:component){
+                    edgeCount+=adj.get(node).size();
+                }
+                edgeCount/=2;
+             int size = component.size();
+             int req = size*(size-1)/2;
+
+             if(req==edgeCount){
+                ans++;
+             }
+
+
+            }
+        }
+        return ans;
+    }
+}
